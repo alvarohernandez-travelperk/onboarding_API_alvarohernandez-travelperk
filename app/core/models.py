f362a7261c0c302b111ddeb6b19641d3734b4f6e
alvarohernandez-travelperk <alvaro.hernandez@travelperk.com>
2022-09-30 12:15:17 +0200

    Add model and recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,17 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
+
+from django.conf import settings
+from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    """Recipe Object"""
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,18 @@
+'''
+Test for models.
+'''
+from django.test import TestCase
+
+from core import models
+
+
+class ModelTests(TestCase):
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        recipe = models.Recipe.objects.create(
+            title='Sample recip name',
+            description='Sample recipe description'
+        )
+
+
+        self.assertEqual(str(recipe), recipe.title)
