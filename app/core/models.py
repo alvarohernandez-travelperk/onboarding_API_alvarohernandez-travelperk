35c9d9127f0c59be339669059ec016d216eb2e64
alvarohernandez-travelperk <alvaro.hernandez@travelperk.com>
2022-10-04 13:23:37 +0200

    lint

--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,15 @@ class Recipe(models.Model):
     """Recipe Object"""
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
+
+
+class Ingredient(models.Model):
+    """Ingredients for recipes."""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
