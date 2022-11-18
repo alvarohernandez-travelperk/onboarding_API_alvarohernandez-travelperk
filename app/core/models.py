22419fc1dd26d2aba69c3ad5c6ee4da7cf02b3c0
alvarohernandez-travelperk <alvaro.hernandez@travelperk.com>
2022-11-18 13:40:11 +0100

    Add limit in fields

--- app/core/models.py
+++ app/core/models.py
@@ -10,8 +10,8 @@ from django.db import models
 
 class Recipe(models.Model):
     """Recipe Object"""
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+    title = models.CharField(max_length=20)
+    description = models.TextField(blank=True, max_length=200)
     ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
@@ -20,7 +20,7 @@ class Recipe(models.Model):
 
 class Ingredient(models.Model):
     """Ingredients for recipes."""
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
